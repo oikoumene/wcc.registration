9309793f11e9f6774cd1536471721989d8d7d0a9
glenn <glenn@glenn-Lenovo-G480>
2016-03-14 11:53:52 +0800

    added new files

--- wcc/registration/interfaces.py
+++ wcc/registration/interfaces.py
@@ -1,5 +1,52 @@
 from zope.interface import Interface
+from zope import schema
+from quintagroup.formlib.captcha import Captcha
 
 class IProductSpecific(Interface):
     pass
 
+class IUserApprovedEvent(Interface):
+    pass
+
+class IUserRejectedEvent(Interface):
+    pass
+
+class IUserRegisteredEvent(Interface):
+    pass
+
+class IRegistrationApproval(Interface):
+
+    def get(key):
+        pass
+
+    def add(key, data):
+        pass
+
+    def approve(key):
+        pass
+
+    def reject(key):
+        pass
+    
+class IExtendRegistrationForm(Interface):
+    """Marker interface for my custom registration form
+    """
+
+
+class ExtendRegistrationForm(Interface):
+    
+    kn_jobtitle = schema.TextLine(
+        title=(u'Job Title'),
+        description=u'',
+        required=False,
+        )
+    
+    kn_organization = schema.TextLine(
+        title=(u'Job Organization'),
+        description=u'',
+        required=False,
+        )
+    
+    captcha = Captcha(
+        title=(u'Type the code'),
+        description=(u'Type the code from the picture shown below.'))
